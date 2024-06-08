1e5d3bc3041ee9a556afdaedad59a936ce82e8ea
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2024-06-08 17:37:55 +0600

    fix: testing reverts

--- next.config.js
+++ next.config.js
@@ -62,7 +62,7 @@ module.exports = () => {
   return plugins.reduce((acc, next) => next(acc), {
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    basePath: '/mmhshayer.github.io',
+    // basePath: '/mmhshayer.github.io',
     eslint: {
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
